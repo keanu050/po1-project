6210905f8b865aa708e2b159636dfb40b98f3acb
JabirAfkir <302743465@student.rocmondriaan.nl>
2025-09-25 12:50:52 +0200

    jabirafkir

---
+++ script.js
@@ -0,0 +1,17 @@
+// commentaar
+let naam = prompt("wat is jou naam?");
+alert("hallo" + naam + "!");
+
+
+//function checkAge() {
+   // let leeftijd = prompt("wat is jou leeftijd")
+   // if (leeftijd < 18) {
+   //     checkAge();
+   // } else {
+    //    alert("welkom bij onze website")
+
+  //  }
+//}
+
+
+//checkAge()
